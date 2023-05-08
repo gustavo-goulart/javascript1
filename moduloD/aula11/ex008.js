8048e8bd492f6efb1452a55db6a61fcff7161c1e
Gustavo Goulart <129804650+gustavo-goulart@users.noreply.github.com>
2023-05-08 19:06:52 -0300

    ex008 009 010

---
+++ moduloD/aula11/ex008.js
@@ -0,0 +1,10 @@
+var vel = 72;
+
+console.log(`A velocidade do seu carro é ${vel}Km/h`);
+
+if (vel > 60) {
+  //condição simples
+  console.log("Você ultrapassou a velocidade permitida. MULTADO!");
+}
+
+console.log("Dirija sempre usando cinto de segurança!");
